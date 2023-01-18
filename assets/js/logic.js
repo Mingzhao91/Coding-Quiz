359f9059b29178818f92e944f069ccf61e50a2f9
Mingzhao91 <edwardwhite7@hotmail.com>
2023-01-18 19:47:00 +0000

    show question to user after the quiz starts

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,14 +4,22 @@ import questionsArr from "./questions.js";
 // DOM elements
 const startBtnEl = document.getElementById("start");
 const timeSpanEl = document.getElementById("time");
+const questionBlockEl = document.getElementById("questions");
+const questionTitleEl = document.getElementById("question-title");
+const questionOptionsEl = document.getElementById("choices");
+
 // local variables
-const quizDurationInSec = 5;
+const quizDurationInSec = 75;
+let questionsSelectedArr = []; // store questions that are shown by user
 
 //---------------Event Listeners -----------------------//
 startBtnEl.addEventListener("click", startTheQuiz);
 
 function startTheQuiz() {
+  questionBlockEl.className = questionBlockEl.className.replace("hide", "show");
+
   startTimer();
+  showQuestionToUser();
 }
 
 //---------------Functions -----------------------//
@@ -36,4 +44,52 @@ function startTimer() {
 }
 
 // show question to the user
-function showQuestionToUser() {}
+function showQuestionToUser() {
+  // select a question randomly
+  const randomQuestionIndex = getRandomNumber(questionsArr.length);
+  const randomQuestion = questionsArr[randomQuestionIndex];
+  questionsSelectedArr.push(randomQuestion);
+  // remove selected question so that it wouldn't be shown up again
+  questionsArr.splice(randomQuestionIndex, 1);
+  // console.log("questionsArr: ", questionsArr);
+  // console.log("questionsSelectedArr: ", questionsSelectedArr);
+  // console.log("randomQuestion: ", randomQuestion);
+
+  // put question in h2 tag
+  questionTitleEl.textContent = randomQuestion.question;
+
+  // build options in order list
+  const optionsOrderListEl = document.createElement("ol");
+  // make a copy of options
+  let optionsList = JSON.parse(JSON.stringify(randomQuestion.options));
+  let optionsRandomArr = [];
+
+  // add questions in different order for each quiz
+  while (optionsRandomArr.length !== randomQuestion.options.length) {
+    const randomOptionIndex = getRandomNumber(optionsList.length);
+    const randomOption = optionsList[randomOptionIndex];
+    optionsRandomArr.push(randomOption);
+    optionsList.splice(randomOptionIndex, 1);
+  }
+
+  // console.log("randomQuestion: ", randomQuestion);
+  // console.log("optionsRandomArr: ", optionsRandomArr);
+
+  // append options to the other list
+  optionsRandomArr.forEach((option) => {
+    const optionLiEl = document.createElement("li");
+    optionLiEl.classList.add("option");
+    optionLiEl.textContent = option;
+    optionsOrderListEl.appendChild(optionLiEl);
+  });
+
+  // console.log("questionTitleEl: ", questionTitleEl);
+  // console.log("optionsOrderListEl: ", optionsOrderListEl);
+
+  // append options to choice element in DOM
+  questionOptionsEl.appendChild(optionsOrderListEl);
+}
+
+function getRandomNumber(maxNum) {
+  return Math.floor(Math.random() * maxNum);
+}

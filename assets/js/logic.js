54ec17cb35cd9382762677683798c87d885cb197
Mingzhao91 <edwardwhite7@hotmail.com>
2023-01-18 18:42:58 +0000

    listen to the start button click and start the timer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,32 @@
 import questionsArr from "./questions.js";
 
-console.log(questionsArr);
+// Variables are declared here
+const startBtnEl = document.getElementById("start");
+const timeSpanEl = document.getElementById("time");
+const quizDurationInSec = 5;
+
+startBtnEl.addEventListener("click", startTheQuiz);
+
+function startTheQuiz() {
+  startTimer();
+}
+
+// start the countdown timer
+function startTimer() {
+  // initilase the duration for the quiz
+  let timeLeft = quizDurationInSec;
+  // update text for the countdown in the DOM
+  timeSpanEl.textContent = quizDurationInSec;
+
+  const quizInterval = setInterval(function () {
+    if (timeLeft > 0) {
+      // decrement the coutdown and show to the user
+      timeLeft--;
+      timeSpanEl.textContent = timeLeft;
+    } else {
+      // clear the interval and navigate to the score page
+      clearInterval(quizInterval);
+      window.location.href = "highscores.html";
+    }
+  }, 1000);
+}

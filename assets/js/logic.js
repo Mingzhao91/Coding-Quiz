beb7fec40681bd3523d0ce1a31fa521a0bfe5a07
Mingzhao91 <edwardwhite7@hotmail.com>
2023-01-18 22:12:42 +0000

    handle user answer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,10 +8,13 @@ const timeSpanEl = document.getElementById("time");
 const questionBlockEl = document.getElementById("questions");
 const questionTitleEl = document.getElementById("question-title");
 const questionOptionsEl = document.getElementById("choices");
+const feedbackEl = document.getElementById("feedback");
 
 // local variables
-const quizDurationInSec = 75;
+const wrongAnswerTimePenalty = 15;
+let quizDurationInSec = 75;
 let questionsSelectedArr = []; // store questions that are shown by user
+let currentQuestion = null; // store the question that is currently shown to the user
 
 //---------------Event Listeners -----------------------//
 startBtnEl.addEventListener("click", startTheQuiz);
@@ -32,69 +35,118 @@ function updateLayoutOnStart() {
 // start the countdown timer
 function startTimer() {
   // initilase the duration for the quiz
-  let timeLeft = quizDurationInSec;
+  // let timeLeft = quizDurationInSec;
   // update text for the countdown in the DOM
   timeSpanEl.textContent = quizDurationInSec;
 
   const quizInterval = setInterval(function () {
-    if (timeLeft > 0) {
+    if (quizDurationInSec > 0) {
       // decrement the coutdown and show to the user
-      timeLeft--;
-      timeSpanEl.textContent = timeLeft;
+      quizDurationInSec--;
+      timeSpanEl.textContent = quizDurationInSec;
     } else {
       // clear the interval and navigate to the score page
       clearInterval(quizInterval);
-      window.location.href = "highscores.html";
+      endQuiz();
     }
   }, 1000);
 }
 
-// show question to the user
-function showQuestionToUser() {
-  // select a question randomly
-  const randomQuestionIndex = getRandomNumber(questionsArr.length);
-  const randomQuestion = questionsArr[randomQuestionIndex];
-  questionsSelectedArr.push(randomQuestion);
-  // remove selected question so that it wouldn't be shown up again
-  questionsArr.splice(randomQuestionIndex, 1);
-  // console.log("questionsArr: ", questionsArr);
-  // console.log("questionsSelectedArr: ", questionsSelectedArr);
-  // console.log("randomQuestion: ", randomQuestion);
+// clear an answer result in DOM
+function clearAnswerResult() {
+  feedbackEl.textContent = "";
+  feedbackEl.classList.add("hide");
+}
 
-  // put question in h2 tag
-  questionTitleEl.textContent = randomQuestion.question;
+// check answer and show the result
+function onOptionClick(event) {
+  // hide previous result
+  clearAnswerResult();
+  // get option from li dataset
+  const selectedOption = event.target.dataset.option;
+  // console.log("selectedOption: ", selectedOption);
 
-  // build options in order list
-  const optionsOrderListEl = document.createElement("ol");
-  // make a copy of options
-  let optionsList = JSON.parse(JSON.stringify(randomQuestion.options));
-  let optionsRandomArr = [];
+  // check if the user gives the correct answer
+  const isCorrect = currentQuestion.answer === selectedOption;
+  // console.log("isCorrect: ", isCorrect);
 
-  // add questions in different order for each quiz
-  while (optionsRandomArr.length !== randomQuestion.options.length) {
-    const randomOptionIndex = getRandomNumber(optionsList.length);
-    const randomOption = optionsList[randomOptionIndex];
-    optionsRandomArr.push(randomOption);
-    optionsList.splice(randomOptionIndex, 1);
+  // deduct time as penalty if wrong answer is given
+  if (!isCorrect) {
+    quizDurationInSec -= wrongAnswerTimePenalty;
   }
 
-  // console.log("randomQuestion: ", randomQuestion);
-  // console.log("optionsRandomArr: ", optionsRandomArr);
+  // show message to say if answer is right or not
+  feedbackEl.textContent = isCorrect ? "Correct!" : "Wrong!";
+  feedbackEl.classList.remove("hide");
 
-  // append options to the other list
-  optionsRandomArr.forEach((option) => {
-    const optionLiEl = document.createElement("li");
-    optionLiEl.classList.add("option");
-    optionLiEl.textContent = option;
-    optionLiEl.dataset.option = option;
-    optionsOrderListEl.appendChild(optionLiEl);
-  });
+  // show new question to the user if there's any, otherwise, end the quiz
+  if (questionsArr.length > 0) {
+    showQuestionToUser();
+  } else {
+    endQuiz();
+  }
+}
 
-  // console.log("questionTitleEl: ", questionTitleEl);
-  // console.log("optionsOrderListEl: ", optionsOrderListEl);
+// show question to the user
+function showQuestionToUser() {
+  // remove all question and its options from the last one
+  questionTitleEl.textContent = "";
+  questionOptionsEl.innerHTML = "";
 
-  // append options to choice element in DOM
-  questionOptionsEl.appendChild(optionsOrderListEl);
+  // show question to user if there's remaning.
+  if (questionsArr.length > 0) {
+    // select a question randomly
+    const randomQuestionIndex = getRandomNumber(questionsArr.length);
+    const randomQuestion = questionsArr[randomQuestionIndex];
+    questionsSelectedArr.push(randomQuestion);
+    // remove selected question so that it wouldn't be shown up again
+    questionsArr.splice(randomQuestionIndex, 1);
+    // console.log("questionsArr: ", questionsArr);
+    // console.log("questionsSelectedArr: ", questionsSelectedArr);
+    // console.log("randomQuestion: ", randomQuestion);
+
+    // put question in h2 tag
+    questionTitleEl.textContent = randomQuestion.question;
+
+    // build options in order list
+    const optionsOrderListEl = document.createElement("ol");
+    // make a copy of options
+    let optionsList = JSON.parse(JSON.stringify(randomQuestion.options));
+    let optionsRandomArr = [];
+
+    // add questions in different order for each quiz
+    while (optionsRandomArr.length !== randomQuestion.options.length) {
+      const randomOptionIndex = getRandomNumber(optionsList.length);
+      const randomOption = optionsList[randomOptionIndex];
+      optionsRandomArr.push(randomOption);
+      optionsList.splice(randomOptionIndex, 1);
+    }
+
+    // console.log("randomQuestion: ", randomQuestion);
+    // console.log("optionsRandomArr: ", optionsRandomArr);
+
+    // append options to the other list
+    optionsRandomArr.forEach((option) => {
+      const optionLiEl = document.createElement("li");
+      optionLiEl.classList.add("option");
+      optionLiEl.textContent = option;
+      optionLiEl.dataset.option = option;
+      optionLiEl.addEventListener("click", onOptionClick);
+      optionsOrderListEl.appendChild(optionLiEl);
+    });
+
+    // console.log("questionTitleEl: ", questionTitleEl);
+    // console.log("optionsOrderListEl: ", optionsOrderListEl);
+
+    // append options to choice element in DOM
+    questionOptionsEl.appendChild(optionsOrderListEl);
+    // store the question that the user needs to answser
+    currentQuestion = JSON.parse(JSON.stringify(randomQuestion));
+  }
+}
+
+function endQuiz() {
+  window.location.href = "highscores.html";
 }
 
 function getRandomNumber(maxNum) {

aeb41c0f920795fdfbd04103ec89f470478dde3e
Mingzhao91 <edwardwhite7@hotmail.com>
2023-01-19 00:18:42 +0000

    if scores are equal, sort by name alphabetically

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -10,9 +10,13 @@ const quizLocalStorageKey = "codeQuiz"; // key for the local storage
 clearBtnEl.addEventListener("click", clearHighScores);
 
 //-------------------------------------------------- Functions --------------------------------------------------//
-// sort by score in descending order
+// sort by score in descending order. if scores are equal, sort by name alphabetically
 function sortScoreBoardDesc(item1, item2) {
-  return item2.score - item1.score;
+  if (item2.score !== item1.score) {
+    return item2.score - item1.score;
+  } else {
+    return item1.name.localeCompare(item2.name);
+  }
 }
 
 // clear local storage for this application
@@ -37,7 +41,7 @@ function showScores() {
     Array.isArray(storageObj.scoreBoard) &&
     storageObj.scoreBoard.length > 0
   ) {
-    // sort array by score in descending order
+    // sort array by score in descending order, if scores are equal, sort by name alphabetically
     storageObj.scoreBoard.sort(sortScoreBoardDesc);
     // insert each item(user name and score) into order list
     storageObj.scoreBoard.forEach((item) => {
